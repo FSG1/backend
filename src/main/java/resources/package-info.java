922c57a9c9e715422751795287c044d7981469ad
Loek Ehren <loek-ehren@hotmail.com>
2017-10-05 16:40:41 +0200

    Add package info files

---
+++ src/main/java/resources/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains all the resources for the REST API.
+ */
+package resources;
---
+++ src/main/java/services/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all the service classes. These are the classes that the resources use to communicate
+ * with the database.
+ */
+package services;
