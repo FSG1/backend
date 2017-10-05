bdb61e81061b689a335c709abdb40691d3734d6d
Tobias Derksen <t.derksen@gmx.de>
2017-10-05 15:51:30 +0200

    Changed server to listen on shutdown hook rather than stdin

--- src/main/java/org/fsg1/fmms/backend/resources/Main.java
+++ src/main/java/org/fsg1/fmms/backend/resources/Main.java
@@ -1,26 +1,22 @@
-package org.fsg1.fmms.backend.resources;
+package resources;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.inject.Singleton;
 import java.io.IOException;
 import java.net.URI;
 
 /**
  * Main class.
+ *
  */
-public final class Main {
+public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    static final String BASE_URI = "http://0.0.0.0:8080/fmms/";
-
-    private Main() {
-    }
+    static final String BASE_URI = "http://localhost:8080/fmms/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     *
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
@@ -36,6 +32,8 @@ public final class Main {
         rc.register(di);
         rc.packages("resources");
 
+        System.out.println("Server listen on: " + config.getServerString());
+
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(config.getServerString()), rc);
@@ -43,16 +41,30 @@ public final class Main {
 
     /**
      * Main method.
-     *
-     * @param args Arguments.
-     * @throws IOException If the server could not start.
+     * @param args
+     * @throws IOException
      */
-    public static void main(final String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Booting server ...");
+
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+
+
+        // register shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Stopping server ...");
+            server.shutdownNow();
+        }, "shutdownHook"));
+
+        // run
+        try {
+            System.out.println("Starting server ...");
+            server.start();
+            System.out.println("Press CTRL^C to exit..");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
     }
 }
 
