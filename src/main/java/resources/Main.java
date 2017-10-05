68efc4bcf8fe5e4bbcfd9803ac558132d25e8369
Tobias Derksen <t.derksen@gmx.de>
2017-10-05 15:51:30 +0200

    Changed server to listen on shutdown hook rather than stdin

--- src/main/java/resources/Main.java
+++ src/main/java/resources/Main.java
@@ -4,7 +4,6 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.inject.Singleton;
 import java.io.IOException;
 import java.net.URI;
 
@@ -14,7 +13,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    static final String BASE_URI = "http://0.0.0.0:8080/fmms/";
+    static final String BASE_URI = "http://localhost:8080/fmms/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -33,6 +32,8 @@ public class Main {
         rc.register(di);
         rc.packages("resources");
 
+        System.out.println("Server listen on: " + config.getServerString());
+
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(config.getServerString()), rc);
@@ -44,11 +45,26 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+        System.out.println("Booting server ...");
+
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+
+
+        // register shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Stopping server ...");
+            server.shutdownNow();
+        }, "shutdownHook"));
+
+        // run
+        try {
+            System.out.println("Starting server ...");
+            server.start();
+            System.out.println("Press CTRL^C to exit..");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
     }
 }
 
