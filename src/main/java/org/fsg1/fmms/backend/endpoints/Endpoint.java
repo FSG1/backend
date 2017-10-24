277f970f317dbdd7f0303ed4cf3858dc8ba90135
Loek Ehren <loek-ehren@hotmail.com>
2017-10-24 13:10:39 +0200

    Added documentation to Endpoint.

--- src/main/java/org/fsg1/fmms/backend/endpoints/Endpoint.java
+++ src/main/java/org/fsg1/fmms/backend/endpoints/Endpoint.java
@@ -8,16 +8,22 @@ import org.fsg1.fmms.backend.services.Service;
 public abstract class Endpoint {
     private final Service service;
 
-    public Service getService() {
-        return service;
-    }
-
     /**
-     * Constructor which receives the service as dependency.
+     * Constructor which receives the service as dependency. In subclasses this dependency is automatically
+     * injected by Jersey's DPI system.
      *
      * @param service Service object.
      */
     Endpoint(final Service service) {
         this.service = service;
+    }
+
+    /**
+     * Gets the service of the Endpoint.
+     *
+     * @return the service.
+     */
+    public Service getService() {
+        return service;
     }
 }
