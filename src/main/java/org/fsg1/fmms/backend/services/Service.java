a60c47c337194c2a80ba35c10759941cfa29f3b3
Loek Ehren <loek-ehren@hotmail.com>
2017-10-25 09:40:02 +0200

    Modified checkstyle and fixed style

--- src/main/java/org/fsg1/fmms/backend/services/Service.java
+++ src/main/java/org/fsg1/fmms/backend/services/Service.java
@@ -29,6 +29,7 @@ public abstract class Service {
     /**
      * Execute a retrieval query on the database with any parameters.
      *
+     * @param query Query string to perform.
      * @param parameters Optional array of parameters to give to the query
      * @return The result of the query in JSON format.
      * @throws SQLException if the query was malformed.
