6e2ace4ece54ae7f33b26ddf66871ec8cc9663b5
Loek Ehren <loek-ehren@hotmail.com>
2017-11-02 13:49:21 +0100

    Add test file

---
+++ src/test/java/org/fsg1/fmms/backend/exceptions/AppExceptionMapperTest.java
@@ -0,0 +1,29 @@
+package org.fsg1.fmms.backend.exceptions;
+
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppExceptionMapperTest {
+    @Test
+    public void testAppException(){
+        final AppException exception = new AppException(312, "Something happened", "Notify the administrator");
+        AppExceptionMapper mapper = new AppExceptionMapper();
+        final Response response = mapper.toResponse(exception);
+        assertEquals(response.getStatus(), 312);
+        final Object entity = response.getEntity();
+        assertEquals(entity, exception);
+    }
+
+    @Test
+    public void testException(){
+        final Exception exception = new Exception("Nobody could have expected this");
+        AppExceptionMapper mapper = new AppExceptionMapper();
+        final Response response = mapper.toResponse(exception);
+        assertEquals(response.getStatus(), 500);
+        final Object entity = response.getEntity();
+        assertEquals(entity, exception);
+    }
+}
