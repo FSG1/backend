42b7fcecf537f95233fc768ae66e0748afd1965b
Loek Ehren <loek-ehren@hotmail.com>
2017-11-09 13:16:42 +0100

    Add qualifications service without query for now

--- src/main/java/org/fsg1/fmms/backend/services/QualificationsService.java
+++ src/main/java/org/fsg1/fmms/backend/services/QualificationsService.java
@@ -1,4 +1,18 @@
 package org.fsg1.fmms.backend.services;
 
-public class QualificationsService {
+import org.fsg1.fmms.backend.database.Connection;
+
+import javax.inject.Inject;
+
+public class QualificationsService extends Service {
+
+    @Inject
+    QualificationsService(Connection connection) {
+        super(connection);
+    }
+
+    public String getQualificationsQuery() {
+        return
+                "";
+    }
 }
