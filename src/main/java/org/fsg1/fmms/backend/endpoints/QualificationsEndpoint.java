0f9bd220a15612b30308beb0a3bf1b429108c75b
Loek Ehren <loek-ehren@hotmail.com>
2017-11-09 13:31:09 +0100

    Modify endpoint

--- src/main/java/org/fsg1/fmms/backend/endpoints/QualificationsEndpoint.java
+++ src/main/java/org/fsg1/fmms/backend/endpoints/QualificationsEndpoint.java
@@ -12,7 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("sp/qualifications")
+@Path("qualifications")
 public class QualificationsEndpoint extends Endpoint {
     /**
      * Constructor which receives the service as dependency.
