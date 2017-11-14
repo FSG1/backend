84fed8d0827ec0a297c57c8f8b39536206d44419
Loek Ehren <loek-ehren@hotmail.com>
2017-11-14 13:30:37 +0100

    Add connection pool
    Add DBCP to add connection pooling and reconnect functionality.

--- src/main/java/org/fsg1/fmms/backend/app/AppBinder.java
+++ src/main/java/org/fsg1/fmms/backend/app/AppBinder.java
@@ -1,5 +1,6 @@
 package org.fsg1.fmms.backend.app;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.fsg1.fmms.backend.database.Connection;
 import org.fsg1.fmms.backend.services.CurriculaService;
 import org.fsg1.fmms.backend.services.CurriculumService;
@@ -19,6 +20,7 @@ public final class AppBinder extends AbstractBinder {
      */
     @Override
     protected void configure() {
+        bind(BasicDataSource.class).to(BasicDataSource.class).in(Singleton.class);
         bind(CurriculumService.class).to(CurriculumService.class).in(Singleton.class);
         bind(CurriculaService.class).to(CurriculaService.class).in(Singleton.class);
         bind(Connection.class).to(Connection.class).in(Singleton.class);
