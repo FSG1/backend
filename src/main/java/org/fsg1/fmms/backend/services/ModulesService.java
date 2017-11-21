5248c16d29eb3354d7cbe8489cc5a9d75ec400a2
Loek Ehren <loek-ehren@hotmail.com>
2017-11-21 14:08:17 +0100

    Change get module query

--- src/main/java/org/fsg1/fmms/backend/services/ModulesService.java
+++ src/main/java/org/fsg1/fmms/backend/services/ModulesService.java
@@ -26,40 +26,42 @@ public class ModulesService extends Service {
     public String getQueryModuleInformation() {
         return
                 "WITH " +
-                        "    prior AS (SELECT Json_build_object('module_code',m.code , 'module_name', m.name, 'type', (CASE WHEN md.mandatory = TRUE THEN 'mandatory' WHEN md.concurrent THEN 'concurrent' ELSE 'previous' END), 'remarks', Coalesce(md.remarks, '')) AS prior_modules, md.module_id AS MODULE FROM study.moduledependency AS md inner join study.MODULE AS m ON m.id = md.dependency_module_id), " +
-                        "    alrow AS (SELECT Row_number() over () AS num, id FROM study.architecturallayer), " +
-                        "    acrow AS (SELECT Row_number() over () AS num, id FROM study.activity), " +
-                        "    astype AS (SELECT Array_to_json(Array_agg(DISTINCT at.name)) AS names, as2lg.learninggoal_id AS lg FROM study.moduleasssementtype AS AT inner join study.moduleassessment_moduleassessmenttype AS ma2at ON ma2at.moduleassessmenttype_id = at.id inner join study.moduleassessment_learninggoal AS as2lg ON ma2at.moduleassessment_id = as2lg.moduleassessment_id GROUP BY as2lg.learninggoal_id), " +
-                        "    skills AS (SELECT Array_to_json(Array_agg(Json_build_object('architectural_layer', (SELECT (num - 1) FROM alrow WHERE alrow.id = q.architecturallayer_id), 'lifecycle_activity', (SELECT (num - 1) FROM acrow WHERE acrow.id = q.activity_id), 'level', los.LEVEL))) AS json, lq.learninggoal_id FROM study.learninggoal_qualification AS lq inner join study.qualification AS q ON q.id = lq.qualification_id inner join study.levelofskill AS los ON los.id = q.levelofskill_id GROUP BY lq.learninggoal_id), " +
-                        "    lg AS (SELECT Array_to_json(Array_agg(Json_build_object('name', Concat('LG ', sequenceno), 'description', description, 'type', (CASE lg.groupgoal WHEN TRUE THEN 'group' ELSE 'personal' END), 'weight', weight, 'assesment_types', (SELECT Coalesce(names, '[]'::json) FROM astype WHERE lg = lg.id), 'skillmatrix', Coalesce((SELECT json FROM skills WHERE skills.learninggoal_id = lg.id), '[]'::json)))) AS json, lg.module_id FROM study.learninggoal AS lg GROUP BY module_id), " +
-                        "    activities AS (SELECT Array_to_json(Array_agg(Json_build_object('lifecycle_activity_id', id, 'lifecycle_activity_name', name, 'lifecycle_activity_description', description))) AS json FROM study.activity), " +
-                        "    als AS (SELECT Array_to_json(Array_agg(Json_build_object('architectural_layer_id', id, 'architectural_layer_name', name, 'architectural_layer_description', description))) AS json FROM study.architecturallayer), " +
-                        "    topics AS (SELECT array_to_json(array_agg(t.description order BY t.sequenceno)) AS topics, t.module_id AS MODULE FROM study.moduletopic AS t GROUP BY t.module_id), " +
-                        "    moduleskills AS (SELECT lg.module_id AS MODULE, json_build_object('lifecycle_activity', (SELECT (num - 1) FROM acrow WHERE id = q.activity_id), 'architectural_layer', (SELECT (num - 1) FROM alrow WHERE id = q.architecturallayer_id), 'level', max(los.LEVEL)) AS json FROM study.learninggoal AS lg inner join study.learninggoal_qualification AS lg2q ON lg2q.learninggoal_id = lg.id inner join study.qualification AS q ON lg2q.qualification_id = q.id inner join study.levelofskill AS los ON los.id = q.levelofskill_id GROUP BY lg.module_id, q.activity_id, q.architecturallayer_id), " +
-                        "    lecturers AS (SELECT array_to_json(array_agg(concat(e.firstname, ' ', e.lastname))) AS json, me.module_id AS MODULE FROM study.module_employee AS me inner join study.employee AS e ON e.id = me.employee_id GROUP BY me.module_id) " +
-                        "SELECT json_build_object( " +
-                        "  'module_code', m.code, " +
-                        "  'module_name', m.name, " +
+                        "    prior as (select json_build_object('code',m.code , 'name', m.name, 'type', (case when md.mandatory = true then 'mandatory' when md.concurrent then 'concurrent' else 'previous' end), 'remarks', coalesce(md.remarks, '')) as prior_modules, md.module_id as module from study.moduledependency as md inner join study.module as m on m.id = md.dependency_module_id), " +
+                        "    alrow as (select row_number() over () as num, id from study.architecturallayer), " +
+                        "    acrow as (select row_number() over () as num, id from study.activity), " +
+                        "    material as (select array_agg(tm.description) as descs, tm.moduledescription_id as md_id from study.teachingmaterial as tm group by tm.moduledescription_id), " +
+                        "    skills as (select array_to_json(array_agg(json_build_object('architectural_layer', (select (num - 1) from alrow where alrow.id = q.architecturallayer_id), 'lifecycle_activity', (select (num - 1) from acrow where acrow.id = q.activity_id), 'level', los.level))) as json, lq.learninggoal_id from study.learninggoal_qualification as lq inner join study.qualification as q on q.id = lq.qualification_id inner join study.levelofskill as los on los.id = q.levelofskill_id group by lq.learninggoal_id), " +
+                        "    lg as (select array_to_json(array_agg(json_build_object('name', concat('LG ', sequenceno), 'description', description, 'type', (case lg.groupgoal when true then 'group' else 'personal' end), 'skillmatrix', COALESCE((select json from skills where skills.learninggoal_id = lg.id), '[]'::JSON)))) as json, lg.module_id from study.learninggoal as lg group by module_id), " +
+                        "    acitivies as (select array_to_json(array_agg(json_build_object('id', id, 'name', name, 'description', description))) as json from study.activity), " +
+                        "    als as (select array_to_json(array_agg(json_build_object('id', id, 'name', name, 'description', description))) as json from study.architecturallayer), " +
+                        "    topics as (select array_to_json(array_agg(t.description order by t.sequenceno)) as topics, t.module_id as module from study.moduletopic as t group by t.module_id), " +
+                        "    moduleskills as (select lg.module_id as module, json_build_object('lifecycle_activity', (select (num - 1) from acrow where id = q.activity_id), 'architectural_layer', (select (num - 1) from alrow where id = q.architecturallayer_id), 'level', max(los.level)) as json from study.learninggoal as lg inner join study.learninggoal_qualification as lg2q on lg2q.learninggoal_id = lg.id inner join study.qualification as q on lg2q.qualification_id = q.id inner join study.levelofskill as los on los.id = q.levelofskill_id group by lg.module_id, q.activity_id, q.architecturallayer_id), " +
+                        "    lecturers as (select array_to_json(array_agg(CONCAT(e.firstname, ' ', e.lastname))) as json, me.module_id as module from study.module_employee as me inner join study.employee as e on e.id = me.employee_id group by me.module_id), " +
+                        "    grading as (select array_agg(json_build_object('subcode', ma.code, 'description', ma.description, 'percentage', coalesce(ma.weight, 0.0), 'minimal_grade', ma.minimumgrade, 'remark', coalesce(ma.remarks, '')) order by ma.code) as json, ma.module_id as module from study.moduleassessment as ma group by ma.module_id) " +
+                        "select json_build_object( " +
+                        "  'code', m.code, " +
+                        "  'name', m.name, " +
                         "  'credits', m.credits, " +
                         "  'credentials', coalesce(md.credentials, ''), " +
-                        "  'lecturers', (SELECT json FROM lecturers WHERE MODULE = m.id), " +
-                        "  'lifecycle_activities', (SELECT json FROM activities), " +
-                        "  'architectural_layers', (SELECT json FROM als), " +
-                        "  'learning_goals', (SELECT json FROM lg WHERE lg.module_id = m.id), " +
+                        "  'lecturers', (select json from lecturers where module = m.id), " +
+                        "  'lifecycle_activities', (select json from acitivies), " +
+                        "  'architectural_layers', (select json from als), " +
+                        "  'learning_goals', (select json from lg where lg.module_id = m.id), " +
                         "  'lectures_in_week', m.lecturesperweek, " +
                         "  'practical_hours_week', m.practicalperweek, " +
                         "  'total_effort', m.totaleffort, " +
                         "  'introductorytext', coalesce(md.introduction, ''), " +
-                        "  'teaching_material', coalesce(md.teachingmaterial, ''), " +
                         "  'additional_information', coalesce(md.additionalinfo, ''), " +
-                        "  'topics', (SELECT coalesce(topics, '[]'::json) FROM topics WHERE MODULE = m.id), " +
+                        "  'topics', (select coalesce(topics, '[]'::JSON) from topics where module = m.id), " +
                         "  'semester', mp.semester, " +
-                        "  'prior_knowledge_references', (SELECT coalesce(array_to_json(array_agg(PRIOR.prior_modules)), '[]'::json) FROM PRIOR WHERE PRIOR.MODULE = m.id), " +
-                        "  'qualifications', (SELECT array_to_json(array_agg(json)) FROM moduleskills WHERE MODULE = m.id) " +
-                        ") as module FROM study.module AS m " +
-                        "  left join study.moduledescription AS md ON md.module_id = m.id " +
-                        "  left join study.module_profile AS mp ON mp.module_id = m.id " +
-                        "  left join study.PROFILE AS p ON mp.profile_id = p.id " +
-                        "WHERE m.code = ? AND p.studyprogramme_id = ?";
+                        "  'teaching_material', (select array_to_json(mat.descs) from material as mat where mat.md_id = md.id), " +
+                        "  'prior_knowledge_references', (select coalesce(array_to_json(array_agg(prior.prior_modules)), '[]'::JSON) from prior where prior.module = m.id), " +
+                        "  'qualifications', (select array_to_json(array_agg(json)) from moduleskills where module = m.id), " +
+                        "  'assesment_parts', (select json from grading where module = m.id) " +
+                        ") from study.module as m " +
+                        "  left join study.moduledescription as md on md.module_id = m.id " +
+                        "  left join study.module_profile as mp on mp.module_id = m.id " +
+                        "  left join study.profile as p on mp.profile_id = p.id " +
+                        "where m.code = ? AND p.studyprogramme_id = ?;";
     }
 }
