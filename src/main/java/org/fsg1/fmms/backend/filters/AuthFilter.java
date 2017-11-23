4d639871188028639d68b7a7625ffb46aebe8e26
Tobias Derksen <t.derksen@gmx.de>
2017-11-23 15:11:36 +0100

    Implemented Auth Filter to secure all URI which starts with "restricted/"

--- src/main/java/org/fsg1/fmms/backend/filters/AuthFilter.java
+++ src/main/java/org/fsg1/fmms/backend/filters/AuthFilter.java
@@ -1,58 +1,64 @@
 package org.fsg1.fmms.backend.filters;
 
+import org.fsg1.fmms.backend.exceptions.UnauthorizedException;
+
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * Authentication Filters.
+ * Enforces authentication for secured endpoints
+ *
+ * @author Tobias Derksen
+ */
 @Provider
 @PreMatching
 public class AuthFilter implements ContainerRequestFilter {
+
     /**
-     * Apply the filter : check input request, validate or not with user auth
-     * @param containerRequest The request from Tomcat server
+     * Regular Expression to detect a valid Authorization header
+     */
+    private static final String REGEX = "^Basic\\s([A-Za-z0-9+/=-]+={0,2})$";
+
+    /**
+     * Apply the filter : check input request, validate or not with user auth.
+     * @param containerRequest The request from server
      */
     @Override
-    public void filter(ContainerRequestContext containerRequest) throws WebApplicationException {
-        /*
-        //GET, POST, PUT, DELETE, ...
-        String method = containerRequest.getMethod();
-        // myresource/get/56bCA for example
+    public void filter(final ContainerRequestContext containerRequest) throws WebApplicationException {
         String path = containerRequest.getUriInfo().getPath(true);
 
-        //We do allow wadl to be retrieve
-        if(method.equals("GET") && (path.equals("application.wadl") || path.equals("application.wadl/xsd0.xsd")){
-            return;
+        if (path.startsWith("restricted/")) {
+            String header = containerRequest.getHeaderString("Authorization");
+            if (header != null && !header.isEmpty()) {
+                Pattern pattern = Pattern.compile(REGEX);
+                Matcher m = pattern.matcher(header.trim());
+
+                if (m.find()) {
+                    String base64 = m.group(1);
+                    String decoded = new String(Base64.getDecoder().decode(base64));
+                    System.out.println(decoded);
+                    String[] credentials = decoded.split(":");
+
+                    if (credentials.length == 2) {
+                        String username = credentials[0];
+                        String password = credentials[1];
+
+                        System.out.println(username);
+                        System.out.println(password);
+
+                        return;
+                    }
+                }
+            }
+
+            throw new UnauthorizedException();
         }
-
-        //Get the authentification passed in HTTP headers parameters
-        String auth = containerRequest.getHeaderString("authorization");
-
-        //If the user does not have the right (does not provide any HTTP Basic Auth)
-        if(auth == null){
-            throw new WebApplicationException(Status.UNAUTHORIZED);
-        }
-
-        //lap : loginAndPassword
-        String[] lap = BasicAuth.decode(auth);
-
-        //If login or password fail
-        if(lap == null || lap.length != 2){
-            throw new WebApplicationException(Status.UNAUTHORIZED);
-        }
-
-        //DO YOUR DATABASE CHECK HERE (replace that line behind)...
-        User authentificationResult =  AuthentificationThirdParty.authentification(lap[0], lap[1]);
-
-        //Our system refuse login and password
-        if(authentificationResult == null){
-            throw new WebApplicationException(Status.UNAUTHORIZED);
-        }
-
-        //TODO : HERE YOU SHOULD ADD PARAMETER TO REQUEST, TO REMEMBER USER ON YOUR REST SERVICE...
-
-        return;
-        */
     }
 }
